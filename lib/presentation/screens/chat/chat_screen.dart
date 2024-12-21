18f6d59ca6513c5c71fe6d9a4e5218ae324470ad
Elias Esperanza <eliasesperanza261380@gmail.com>
2024-12-20 20:46:51 -0600

    add apartado para mis mensajes

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://th.bing.com/th/id/OIP.GEItiDz7-RhdDGnprxwlQgAAAA?rs=1&pid=ImgDetMain'),
+          ),
+        ),
+        title: const Text('Mi Amor'),
+      ),
+      body: _ChatView()
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return const MyMessageBubble();
+                },
+              ), 
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
